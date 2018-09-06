2cecb40afa10191eeeb0c95e6d6240f100b5813b
minako.sanders <fb030323@yahoo.co.jp>
2018-09-06 01:10:06 +0000

    added add_users_column migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180827163300) do
+ActiveRecord::Schema.define(version: 20180906010808) do
 
   create_table "tasks", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.string   "content"
